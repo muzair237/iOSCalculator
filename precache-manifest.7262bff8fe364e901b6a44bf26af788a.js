68628692ac5da9b581bb20ce1a166db455b40438
Uzair <uzair.ejaz2001@gmail.com>
2023-06-13 00:24:47 +0500

    Updates

---
+++ precache-manifest.7262bff8fe364e901b6a44bf26af788a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d859069484c8c393aae5",
+    "url": "/iOSCalculator/static/css/main.16640d66.chunk.css"
+  },
+  {
+    "revision": "d859069484c8c393aae5",
+    "url": "/iOSCalculator/static/js/main.a003d533.chunk.js"
+  },
+  {
+    "revision": "3982d31cff20eb92f504",
+    "url": "/iOSCalculator/static/js/runtime~main.841c28d6.js"
+  },
+  {
+    "revision": "6cea5c82aaa62ed3b8cb",
+    "url": "/iOSCalculator/static/js/2.2c271fbb.chunk.js"
+  },
+  {
+    "revision": "cf17f5da4b08ae304c39b88106e26a94",
+    "url": "/iOSCalculator/index.html"
+  }
+];
